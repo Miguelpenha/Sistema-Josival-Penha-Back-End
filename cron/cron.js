ad52e316d69e739e49235cdb62e49df6a718b206
Miguel Penha <miguel.penha1@gmail.com>
2021-10-12 06:39:15 -0300

    Ajeitando o cron

--- cron/cron.js
+++ cron/cron.js
@@ -4,13 +4,15 @@ const path = require('path')
 
 const tarefas = fs.readdirSync(path.resolve(__dirname, 'tarefas'))
 tarefas.map(nomeArq => {
-    const tarefa = require(`./tarefas/${nomeArq}`)
-    if (tarefa.exec) {
-        tarefa()
-    }
-    if (tarefa.tempo) {
-        cron.schedule(tarefa.tempo, tarefa)
-    } else {
-        cron.schedule('* * * * * *', tarefa)
+    if (nomeArq.includes('.js')) {
+        const tarefa = require(`./tarefas/${nomeArq}`)
+        if (tarefa.exec) {
+            tarefa()
+        }
+        if (tarefa.tempo) {
+            cron.schedule(tarefa.tempo, tarefa)
+        } else {
+            cron.schedule('* * * * * *', tarefa)
+        }
     }
 })
