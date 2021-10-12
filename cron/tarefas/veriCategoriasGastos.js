430c9616b8a236023370754936043d20a305af4d
Miguel Penha <miguel.penha1@gmail.com>
2021-10-12 03:52:16 -0300

    Tirando arquivo não usado do cron

--- cron/tarefas/veriCategoriasGastos.js
+++
@@ -1,9 +0,0 @@
-const categoriasGastos = require('../../utils/categoriasGastos')
-const categoriasGastosModels = require('../../models/financeiro/despesas/categorias')
-const data = require('../../utils/data')
-
-module.exports = function veriCategoriasGastos() {
-    //console.log('asd')
-}
-
-module.exports.exec = true
