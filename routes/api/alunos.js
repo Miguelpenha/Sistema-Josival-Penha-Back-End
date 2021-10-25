8a6a53dc8ba9b1c9c58f957fdb68482d8954f749
Miguel Penha <miguel.penha1@gmail.com>
2021-10-25 12:56:43 -0300

    Adicionando cadastro de aluno na api

--- routes/api/alunos.js
+++ routes/api/alunos.js
@@ -2,12 +2,19 @@
     const express = require('express')
     const alunos = express.Router()
     const mongoose = require('mongoose')
-    // Middlewares
-        
+    const multer = require('multer')
+    const fs = require('fs')
+    const path = require('path')
+    const data = require('../../utils/data')
+    // Configs
+        const configMulter = require('../../config/multer/multer')
     // Models
         const alunosModels = require('../../models/aluno')
+        const professorasModels = require('../../models/professora')
+        const turmasModels = require('../../models/turma')
 // Confi
-    // Middlewares
+    // Multer
+        const fotoUpload = multer(configMulter.foto)
 // Grupo de rotas
 
 // Rotas solo
@@ -20,6 +27,68 @@
             const alunos = await alunosModels.find({})
 
             res.json(alunos)
+        }
+    })
+
+    alunos.post('/', fotoUpload.single('foto'), async (req, res) => {
+        const { nome, sexo, nascimento, cpf, responsável1, responsável2, telefone, email, cep, cidade, bairro, rua, número, complemento, matrícula, turma, situação, observação, criação } = req.body
+
+        const aluno = await alunosModels.findOne({nome: String(nome)})
+        if (aluno) {
+            res.json({error: 'Já existe um aluno cadastrado com esse nome'})
+        } else {
+            const professora = (await professorasModels.findOne({nome: (await turmasModels.findOne({nome: turma})).professora}))._id
+            let foto = {}
+            if (req.file) {
+                const { originalname: nomeArq, mimetype: tipo, key, size: tamanho, location: url=undefined } = req.file
+                foto = {
+                    nome: nomeArq,
+                    key,
+                    tamanho,
+                    tipo,
+                    url
+                }
+            } else {
+                foto = {
+                    nome: 'Padrão.jpg',
+                    key: 'Padrão.jpg',
+                    tamanho: Number(fs.statSync(path.resolve(__dirname, '..', '..', 'public', 'Padrão.jpg')).size),
+                    tipo: 'image/jpeg',
+                    url: `${process.env.DOMINIO}/public/Padrão.jpg`
+                }
+            }
+            
+            alunosModels.create({
+                nome,
+                sexo,
+                nascimento,
+                cpf,
+                responsável1,
+                responsável2,
+                telefone,
+                email,
+                endereço: {
+                    cep,
+                    número,
+                    complemento,
+                    bairro,
+                    cidade,
+                    rua
+                },
+                matrícula,
+                turma: (await turmasModels.findOne({nome: turma}))._id,
+                professora,
+                situação,
+                observação,
+                foto,
+                criação: {
+                    data: new Date(criação).toLocaleDateString(),
+                    hora: new Date(criação).toLocaleTimeString().split(':')[0]+':'+new Date(criação).toLocaleTimeString().split(':')[1],
+                    sistema: new Date(criação).toISOString()
+                }
+            }).then(() => {
+                res.json({created: true})
+            })
         }
     })
 
