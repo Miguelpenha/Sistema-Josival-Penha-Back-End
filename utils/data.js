4a8c4e4935f3ff8e13439302d27e495818e7b7b5
Miguel Penha <miguel.penha1@gmail.com>
2021-10-27 21:18:49 -0300

    Adicionando a função getMes

--- utils/data.js
+++ utils/data.js
@@ -22,6 +22,34 @@ module.exports.input = () => {
     return `${data.split('/')[2]}-${data.split('/')[1]}-${data.split('/')[0]}`
 }
 
+module.exports.getMes = (mes='') => {
+    if (mes === '01') {
+        return 'janeiro'
+    } else if (mes === '02') {
+        return 'fevereiro'
+    } else if (mes === '03') {
+        return 'março'
+    } else if (mes === '04') {
+        return 'abril'
+    } else if (mes === '05') {
+        return 'maio'
+    } else if (mes === '06') {
+        return 'junho'
+    } else if (mes === '07') {
+        return 'julho'
+    } else if (mes === '08') {
+        return 'agosto'
+    } else if (mes === '09') {
+        return 'setembro'
+    } else if (mes === '10') {
+        return 'outubro'
+    } else if (mes === '11') {
+        return 'novembro'
+    } else if (mes === '12') {
+        return 'dezembro'
+    }
+}
+
 module.exports.converter = {
     inputParaData: dataBruta => {
         const dataSepa = String(dataBruta).split('-')
