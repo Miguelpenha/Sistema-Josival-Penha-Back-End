0753eb223d1d7283cb01aab3f7880c38e42bdd75
Miguel Penha <miguel.penha1@gmail.com>
2021-10-28 02:47:17 -0300

    Terminando rota de gerar pdf

--- routes/api/alunos.js
+++ routes/api/alunos.js
@@ -5,8 +5,9 @@
     const multer = require('multer')
     const fs = require('fs')
     const path = require('path')
-    const PDFPrinter = require('pdfmake')
+    const PDFDOCUMENT = require('pdfkit')
     const data = require('../../utils/data')
+    const axios = require('axios').default
     // Configs
         const configMulter = require('../../config/multer/multer')
     // Models
@@ -107,105 +108,144 @@
         }
     })
 
-    alunos.get('/exportar', async (req, res) => {
-        const alunos = await alunosModels.find({})
+    alunos.get('/exportar/:id/:frequencia', async (req, res) => {
+        const { id, frequencia: frequência } = req.params
+        const aluno = await alunosModels.findById(id)
+        const anoLetivo = (await turmasModels.findById(aluno.turma)).serie
 
-        const fonts = {
-            Helvetica: {
-                normal: 'Helvetica',
-                bold: 'Helvetica-Bold',
-                italics: 'Helvetica-Oblique',
-                bolditalics: 'Helvetica-BoldOblique'
-            }
-        }
-
-        const printer = new PDFPrinter(fonts)
-
-        const body = []
-
-        for await (let aluno of alunos) {
-            const rows = new Array()
-
-            rows.push(aluno.turma)
-            rows.push(aluno.professora)
-            rows.push(aluno.nome)
-            rows.push(aluno.telefone)
-            rows.push(aluno.email)
-
-            body.push(rows)
-        }
-        
-        const pdfDoc = printer.createPdfKitDocument({
-            defaultStyle: { font: 'Helvetica' },
-            content: [
-                {
-                    text: 'Instituto Educacional Josival Penha',
-                    margin: [0, 80, 0, 5],
-                    style: {
-                        bold: true,
-                        alignment: 'center',
-                        fontSize: 11
-                    }
-                },
-                {
-                    text: 'Cadastro Escolar n° P. 109.212 / INEP n° 26170981',
-                    margin: [0, 0, 0, 5],
-                    style: {
-                        alignment: 'center',
-                        fontSize: 11
-                    }
-                },
-                {
-                    text: 'Portaria SEE n° 888 D.O 18/02/2003',
-                    margin: [0, 0, 0, 5],
-                    style: {
-                        alignment: 'center',
-                        fontSize: 11
-                    }
-                },
-                {
-                    text: 'CNPJ: 11.654.198/0001-43',
-                    margin: [0, 0, 0, 5],
-                    style: {
-                        alignment: 'center',
-                        fontSize: 11
-                    }
-                },
-                {
-                    text: 'DECLARAÇÃO',
-                    margin: [0, 55, 0, 45],
-                    style: {
-                        bold: true,
-                        alignment: 'center',
-                        fontSize: 22
-                    }
-                },
-                {
-                    text: `Paulista, ${new Date().getDate()} de ${data.getMes(new Date().toLocaleDateString('pt-br').split('/')[1])} de ${new Date().getFullYear()}`,
-                    margin: [0, 55, 0, 0],
-                    style: {
-                        alignment: 'right',
-                        fontSize: 11
-                    }
-                },
-                {
-                    text: ``,
-                    margin: [0, 0, 0, 0],
-                    style: {
-                        alignment: 'justify',
-                        fontSize: 11
-                    }
-                }
-            ]
-        })
+        const doc = new PDFDOCUMENT({size: 'A4', margin: 60, lang: 'pt-br', displayTitle: `Declaração do aluno(a) ${aluno.nome}`, info: {
+            Title: `Declaração do aluno(a) ${aluno.nome}`,
+            CreationDate: new Date(),
+            Author: 'Sistema Josival Penha',
+            Creator: 'Sistema Josival Penha',
+            ModDate: new Date(),
+            Producer: 'Sistema Josival Penha'
+        }})
 
         const chunks = []
 
-        pdfDoc.on('data', chunk => chunks.push(chunk))
+        doc.name = `Declaração do aluno(a) ${aluno.nome}`
 
-        pdfDoc.end()
+        doc.on('data', chunk => chunks.push(chunk))
+        
+        const { data: assinatura } = await axios.get(process.env.URL_ASSINATURA, {
+            responseType: 'arraybuffer'
+        })
 
-        pdfDoc.on('end', () => res.end(Buffer.concat(chunks)))
+        doc
+        .opacity(0.15)
+        .image(path.resolve(__dirname, '..', '..', 'public', 'logo-josival-penha.png'), 150, 300, {
+            width: 300
+        })
+        .opacity(1)
+        .image(path.resolve(__dirname, '..', '..', 'public', 'logo-josival-penha.png'), 250, 79, {
+            width: 90
+        })
+        .font('Helvetica-Bold')
+        .moveDown(8.4)
+        .text('Instituto Educacional Josival Penha', {
+            align: 'center'
+        })
+        .font('Helvetica')
+        .moveDown(0.4)
+        .text('Cadastro Escolar nº P. 109.212 / INEP nº 26170981', {
+            align: 'center'
+        })
+        .moveDown(0.4)
+        .text('Portaria SEE nº 888 D.O 18/02/2003', {
+            align: 'center'
+        })
+        .moveDown(0.4)
+        .text('CNPJ: 11.654.198/0001-43', {
+            align: 'center'
+        })
+        .fontSize(20)
+        .moveDown(2)
+        .font('Helvetica-Bold')
+        .text('DECLARAÇÃO', {
+            align: 'center'
+        })
+        .font('Helvetica')
+        .fontSize(13)
+        .moveDown(3)
+        .text(`Paulista, ${new Date().getDate()} de ${data.getMes(new Date().toLocaleDateString('pt-br').split('/')[1])} de ${new Date().       getFullYear()}`, {
+            align: 'right'
+        })
+        .moveDown(2)
+        .text('Declaramos para os devidos fins que a aluno(a) ', {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica-Bold')
+        .text(aluno.nome, {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica')
+        .text(', nascido em: ', {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica-Bold')
+        .text(aluno.nascimento, {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica')
+        .text(', Número de Identificação Social (NIS): ', {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica-Bold')
+        .text('236.3487.1004', {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica')
+        .text(', filho de ', {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica-Bold')
+        .text(`${aluno.responsável1} e ${aluno.responsável2}`, {
+            continued: true,
+            align: 'justify'
+        })
+        .font('Helvetica')
+        .text(`, matriculado neste estabelecimento de Ensino no ${anoLetivo} do Ensino Fundamental com bolsa de estudos.`, {
+            align: 'justify'
+        })
+        .moveDown(1.5)
+        .text(`Tem frequência de ${frequência}% dos dias letivos`)
+        .image(assinatura, 230, 610, {
+            width: 140
+        })
+        .moveDown(6.2)
+        .fontSize(12)
+        .text('__________________________________', {
+            align: 'center'
+        })
+        .fontSize(14)
+        .moveDown(0.4)
+        .text('Diretora', {
+            align: 'center'
+        })
+        .font('Helvetica-Bold')
+        .fontSize(11)
+        .text('tel. (81) 3437-2618', 100, 752)
+        .text('Av. João Paulo II, 894', 240, 751)
+        .text('www.josivalpenha.com', 385, 750)
+        .text('cel. (81) 99499-7501', 100, 768)
+        .text('Mirueira, Paulista - PE', 240, 767)
+        .text('@josival.penha', 385, 766)
+
+        doc.end()
+
+        doc.on('end', () => {
+            res.setHeader('Content-disposition', `inline; filename=Declaração do aluno(a) ${aluno.nome}.pdf`)
+            res.setHeader('Content-type', 'application/pdf')
+            res.end(Buffer.concat(chunks))
+        })
     })
 // Exportações
     module.exports = alunos
