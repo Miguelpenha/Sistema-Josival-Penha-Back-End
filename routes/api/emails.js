9b5e5d384549cd3f6c2d20be96d0ee88bff9bb79
Miguel Penha <miguel.penha1@gmail.com>
2022-01-04 22:06:45 -0300

    teste

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -5,10 +5,12 @@ const alunosModels = require('../../models/aluno')
 const fs = require('fs')
 const path = require('path')
 const handlebars = require('handlebars')
-const replaceAll = require('../../utils/replaceAll')
+const markDown = require('markdown-it')({
+    html: true
+})
 
 emails.post('/responsible', async (req, res) => {
-    const { id, msg } = req.body
+    let { id, msg } = req.body
 
     if (mongoose.isValidObjectId(id)) {
         const aluno = await alunosModels.findById(id)
@@ -16,7 +18,7 @@ emails.post('/responsible', async (req, res) => {
         if (aluno) {
             const viewEmail = fs.readFileSync(path.resolve(__dirname, '..', '..', 'views', 'emails', 'responsible.handlebars')).toString()
             const templateEmail = handlebars.compile(viewEmail)
-            const HTMLEmail = templateEmail({ msg: replaceAll(msg, './', '<br>') })
+            const HTMLEmail = templateEmail({ msg: markDown.render(msg) })
             /*sendGrid.send({
                 to: process.env.SENDGRID_EMAIL,
                 from: process.env.SENDGRID_EMAIL,
@@ -27,7 +29,6 @@ emails.post('/responsible', async (req, res) => {
             }).catch(err => {
                 sendEmail = false
             })*/
-            console.log(HTMLEmail)
             res.send(HTMLEmail)
         } else {
             res.json({exists: false})
--- utils/replaceAll.js
+++
@@ -1,3 +0,0 @@
-module.exports = function replaceAll(text, replacer, substitute) {
-    return text.replace(new RegExp(replacer.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'), 'g'), substitute)
-}
