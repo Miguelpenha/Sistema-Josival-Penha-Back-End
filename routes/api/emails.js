bcf6569c98f12fd9d47fde79a694fa3bd803e4ea
Miguel Penha <miguel.penha1@gmail.com>
2022-01-05 07:05:29 -0300

    teste

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -6,30 +6,29 @@ const fs = require('fs')
 const path = require('path')
 const handlebars = require('handlebars')
 const markDown = require('markdown-it')({
-    html: true
+    html: true,
+    breaks: true,
+    typographer: true
 })
+const sendGrid = require('@sendgrid/mail')
 
 emails.post('/responsible', async (req, res) => {
     let { id, msg } = req.body
 
     if (mongoose.isValidObjectId(id)) {
         const aluno = await alunosModels.findById(id)
-
+        
         if (aluno) {
             const viewEmail = fs.readFileSync(path.resolve(__dirname, '..', '..', 'views', 'emails', 'responsible.handlebars')).toString()
             const templateEmail = handlebars.compile(viewEmail)
             const HTMLEmail = templateEmail({ msg: markDown.render(msg) })
-            /*sendGrid.send({
-                to: process.env.SENDGRID_EMAIL,
+
+            sendGrid.send({
+                to: aluno.email,
                 from: process.env.SENDGRID_EMAIL,
                 subject: 'Aviso do Instituto Educacional Josival Penha',
                 html: HTMLEmail
-            }).then(() => {
-                sendEmail = true
-            }).catch(err => {
-                sendEmail = false
-            })*/
-            res.send(HTMLEmail)
+            }).then(() => res.json({send: true})).catch(err => res.json({send: false}))
         } else {
             res.json({exists: false})
         }
