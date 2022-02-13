90b4e347da1ecb3bed6622ecad7bcb9735192053
Miguel Penha <miguel.penha1@gmail.com>
2022-02-12 21:05:41 -0300

    Update index.js

--- routes/api/alunos/index.js
+++ routes/api/alunos/index.js
@@ -295,6 +295,202 @@ alunos.post('/', fotoUpload.single('foto'), async (req, res) => {
     }
 })
 
+alunos.post('/exportar', async (req, res) => {
+    const planilha = new excelJs.Workbook()
+    const pagina = planilha.addWorksheet('Alunos')
+    
+    pagina.columns = [
+        {
+            header: 'Nome: ', 
+            key: 'nome', 
+            width: 25
+        },
+        {
+            header: 'Sexo: ', 
+            key: 'sexo', 
+            width: 25
+        },
+        {
+            header: 'Data de nascimento: ', 
+            key: 'nascimento', 
+            width: 25
+        },
+        {
+            header: 'CPF: ', 
+            key: 'cpf', 
+            width: 25
+        },
+        {
+            header: 'Responsável 1: ', 
+            key: 'responsavel1', 
+            width: 25
+        },
+        {
+            header: 'Responsável 2: ', 
+            key: 'responsavel2',
+            width: 25
+        },
+        {
+            header: 'Telefone: ', 
+            key: 'telefone', 
+            width: 25
+        },
+        {
+            header: 'E-mail: ', 
+            key: 'email', 
+            width: 25
+        },
+        {
+            header: 'CEP: ', 
+            key: 'endereco.cep', 
+            width: 25
+        },
+        {
+            header: 'Cidade: ', 
+            key: 'endereco.cidade', 
+            width: 25
+        },
+        {
+            header: 'Bairro: ', 
+            key: 'endereco.bairro', 
+            width: 25
+        },
+        {
+            header: 'Rua: ', 
+            key: 'endereco.rua', 
+            width: 25
+        },
+        {
+            header: 'Número da casa: ', 
+            key: 'endereco.numero', 
+            width: 25
+        },
+        {
+            header: 'Complemento da casa: ', 
+            key: 'endereco.complemento', 
+            width: 25
+        },
+        {
+            header: 'Data de matrícula: ', 
+            key: 'matricula', 
+            width: 25
+        },
+        {
+            header: 'Turma: ', 
+            key: 'turma', 
+            width: 25
+        },
+        {
+            header: 'Professora: ', 
+            key: 'professora', 
+            width: 25
+        },
+        {
+            header: 'Situação: ', 
+            key: 'situacao', 
+            width: 10
+        },
+        {
+            header: 'Observação: ', 
+            key: 'observacao', 
+            width: 25
+        },
+        {
+            header: 'Foto: ', 
+            key: 'foto.url', 
+            width: 45
+        },
+        {
+            header: 'Data de cadastro no sistema: ', 
+            key: 'criacao.data:criacao.hora', 
+            width: 25
+        }
+    ]
+    
+    const alunosBrutos = await alunosModels.find({})
+    
+    const alunos = await Promise.all(
+        alunosBrutos.map(async aluno => {
+            aluno.turma = (await turmasModels.findById(aluno.turma)).nome
+            aluno.professora = (await professorasModels.findById(aluno.professora)).nome
+
+            return aluno
+        })
+    )
+
+    alunos.map((aluno, index) => {
+        pagina.addRow([
+            aluno.nome || '',
+            aluno.sexo || '',
+            aluno.nascimento || '',
+            aluno.cpf || '',
+            aluno.responsável1 || '',
+            aluno.responsável2 || '',
+            aluno.telefone || '',
+            aluno.email || '',
+            aluno.endereço.cep || '',
+            aluno.endereço.cidade || '',
+            aluno.endereço.bairro || '',
+            aluno.endereço.rua || '',
+            aluno.endereço.número || '',
+            aluno.endereço.complemento || '',
+            aluno.matrícula || '',
+            aluno.turma || '',
+            aluno.professora || '',
+            aluno.situação || '',
+            aluno.observação || '',
+            aluno.foto.url || '',
+            `${aluno.criação.data} ás ${aluno.criação.hora}`
+        ])
+
+        try {
+            pagina.findCell('A'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('B'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('C'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('D'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('E'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('F'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('G'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('H'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('I'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('J'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('K'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('L'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('M'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('N'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('O'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('P'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('Q'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('R'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('S'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('T'+(index+2)).alignment = {horizontal: 'center'}
+            pagina.findCell('T'+(index+2)).value = {
+                text: aluno.foto.url,
+                hyperlink: aluno.foto.url,
+                tooltip: aluno.foto.url
+            }
+            pagina.findCell('U'+(index+2)).alignment = {horizontal: 'center'}
+        } catch {
+
+        }
+    })
+    
+    const caminhoPlanilha = path.resolve(__dirname, '..', '..', '..', 'public', 'planilhas', `${crypto.randomBytes(4).toString('hex')}-alunos.xlsx`)
+
+    await planilha.xlsx.writeFile(caminhoPlanilha)
+    const tamanho = fs.statSync(caminhoPlanilha)
+
+    res.setHeader('Content-Description', 'File Transfer')
+    res.setHeader('Content-Disposition', 'attachment; filename=alunos.xlsx')
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    res.setHeader('Content-Length', tamanho.size)
+    res.setHeader('Content-Transfer-Encoding', 'binary')
+    res.setHeader('Cache-Control', 'must-revalidate')
+    res.setHeader('Pragma', 'public')
+    
+    res.download(caminhoPlanilha, 'alunos.xlsx', () => fs.unlinkSync(caminhoPlanilha))
+})
+
 alunos.post('/:id', async (req, res) => {
     Object.keys(req.body).map(key => req.body[key] = req.body[key] ? req.body[key] : null)
     
@@ -409,202 +605,6 @@ alunos.delete('/:id', async (req, res) => {
     } else {
         res.json({exists: false})
     }
-})
-
-alunos.post('/exportar', async (req, res) => {
-    const planilha = new excelJs.Workbook()
-    const pagina = planilha.addWorksheet('Alunos')
-    
-    pagina.columns = [
-        {
-            header: 'Nome: ', 
-            key: 'nome', 
-            width: 25
-        },
-        {
-            header: 'Sexo: ', 
-            key: 'sexo', 
-            width: 25
-        },
-        {
-            header: 'Data de nascimento: ', 
-            key: 'nascimento', 
-            width: 25
-        },
-        {
-            header: 'CPF: ', 
-            key: 'cpf', 
-            width: 25
-        },
-        {
-            header: 'Responsável 1: ', 
-            key: 'responsavel1', 
-            width: 25
-        },
-        {
-            header: 'Responsável 2: ', 
-            key: 'responsavel2',
-            width: 25
-        },
-        {
-            header: 'Telefone: ', 
-            key: 'telefone', 
-            width: 25
-        },
-        {
-            header: 'E-mail: ', 
-            key: 'email', 
-            width: 25
-        },
-        {
-            header: 'CEP: ', 
-            key: 'endereco.cep', 
-            width: 25
-        },
-        {
-            header: 'Cidade: ', 
-            key: 'endereco.cidade', 
-            width: 25
-        },
-        {
-            header: 'Bairro: ', 
-            key: 'endereco.bairro', 
-            width: 25
-        },
-        {
-            header: 'Rua: ', 
-            key: 'endereco.rua', 
-            width: 25
-        },
-        {
-            header: 'Número da casa: ', 
-            key: 'endereco.numero', 
-            width: 25
-        },
-        {
-            header: 'Complemento da casa: ', 
-            key: 'endereco.complemento', 
-            width: 25
-        },
-        {
-            header: 'Data de matrícula: ', 
-            key: 'matricula', 
-            width: 25
-        },
-        {
-            header: 'Turma: ', 
-            key: 'turma', 
-            width: 25
-        },
-        {
-            header: 'Professora: ', 
-            key: 'professora', 
-            width: 25
-        },
-        {
-            header: 'Situação: ', 
-            key: 'situacao', 
-            width: 10
-        },
-        {
-            header: 'Observação: ', 
-            key: 'observacao', 
-            width: 25
-        },
-        {
-            header: 'Foto: ', 
-            key: 'foto.url', 
-            width: 45
-        },
-        {
-            header: 'Data de cadastro no sistema: ', 
-            key: 'criacao.data:criacao.hora', 
-            width: 25
-        }
-    ]
-    
-    const alunosBrutos = await alunosModels.find({})
-
-    const alunos = await Promise.all(
-        alunosBrutos.map(async aluno => {
-            aluno.turma = (await turmasModels.findById(aluno.turma)).nome
-            aluno.professora = (await professorasModels.findById(aluno.professora)).nome
-
-            return aluno
-        })
-    )
-
-    alunos.map((aluno, index) => {
-        pagina.addRow([
-            aluno.nome || '',
-            aluno.sexo || '',
-            aluno.nascimento || '',
-            aluno.cpf || '',
-            aluno.responsável1 || '',
-            aluno.responsável2 || '',
-            aluno.telefone || '',
-            aluno.email || '',
-            aluno.endereço.cep || '',
-            aluno.endereço.cidade || '',
-            aluno.endereço.bairro || '',
-            aluno.endereço.rua || '',
-            aluno.endereço.número || '',
-            aluno.endereço.complemento || '',
-            aluno.matrícula || '',
-            aluno.turma || '',
-            aluno.professora || '',
-            aluno.situação || '',
-            aluno.observação || '',
-            aluno.foto.url || '',
-            `${aluno.criação.data} ás ${aluno.criação.hora}`
-        ])
-
-        try {
-            pagina.findCell('A'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('B'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('C'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('D'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('E'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('F'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('G'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('H'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('I'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('J'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('K'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('L'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('M'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('N'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('O'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('P'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('Q'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('R'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('S'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('T'+(index+2)).alignment = {horizontal: 'center'}
-            pagina.findCell('T'+(index+2)).value = {
-                text: aluno.foto.url,
-                hyperlink: aluno.foto.url,
-                tooltip: aluno.foto.url
-            }
-            pagina.findCell('U'+(index+2)).alignment = {horizontal: 'center'}
-        } catch {
-
-        }
-    })
-    
-    const caminhoPlanilha = path.resolve(__dirname, '..', '..', '..', 'public', 'planilhas', `${crypto.randomBytes(4).toString('hex')}-alunos.xlsx`)
-
-    await planilha.xlsx.writeFile(caminhoPlanilha)
-    const tamanho = fs.statSync(caminhoPlanilha)
-
-    res.setHeader('Content-Description', 'File Transfer')
-    res.setHeader('Content-Disposition', 'attachment; filename=alunos.xlsx')
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    res.setHeader('Content-Length', tamanho.size)
-    res.setHeader('Content-Transfer-Encoding', 'binary')
-    res.setHeader('Cache-Control', 'must-revalidate')
-    res.setHeader('Pragma', 'public')
-    
-    res.download(caminhoPlanilha, 'alunos.xlsx', () => fs.unlinkSync(caminhoPlanilha))
 })
 
 module.exports = alunos
