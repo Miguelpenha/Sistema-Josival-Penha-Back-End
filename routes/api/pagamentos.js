8a782ffad466c488b21e7664b56c4a12cf9dc47f
Miguel Penha <miguel.penha1@gmail.com>
2022-02-13 16:31:28 -0300

    Update pagamentos.js

--- routes/api/pagamentos.js
+++ routes/api/pagamentos.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const pagamentos = express.Router()
 const alunosModels = require('../../models/aluno')
+const dinero = require('dinero.js')
 
 pagamentos.get('/:id', async (req, res) => {
     const { id } = req.params
@@ -11,11 +12,39 @@ pagamentos.get('/:id', async (req, res) => {
 })
 
 pagamentos.post('/', async (req, res) => {
-    const { id, mês, value, pago, vencimento, forma } = req.body
-
+    let { id, mês, value, pago, vencimento, forma } = req.body
     const pagamentos = (await alunosModels.findById(id)).pagamentos
 
-    res.json(pagamentos[mês])
+    if (pagamentos) {
+        try {
+            value.includes(',') ? null : value = `${value},00`
+            value = Number(
+                value.replace('.', '')
+                .replace(',', '')
+                .replace('R$', '')
+                .trimStart()
+            )
+        } catch {
+            
+        }
+
+        pagamentos[mês] = {
+            value: dinero({ amount: value || 0, currency: 'BRL' }).toFormat(),
+            valueBruto: value || 0,
+            pago,
+            vencimento,
+            vencimentoSistema: new Date(`${vencimento.split('/')[2]}/${vencimento.split('/')[1]}/${vencimento.split('/')[0]}`).toISOString(),
+            forma
+        }
+
+        alunosModels.findByIdAndUpdate(id, {
+            pagamentos
+        })
+        .then(() => res.json({edited: true}))
+        .catch(() => res.json({edited: false}))
+    } else {
+        res.json({exists: false})
+    }
 })
 
 module.exports = pagamentos
