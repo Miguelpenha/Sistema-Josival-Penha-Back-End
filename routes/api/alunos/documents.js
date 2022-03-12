7fb085948fb15ad13d4db8b02dc75a6b4792780b
Miguel Penha <miguel.penha1@gmail.com>
2022-03-12 06:44:01 -0300

    Update documents.js

--- routes/api/alunos/documents.js
+++ routes/api/alunos/documents.js
@@ -10,6 +10,9 @@ const excelJs = require('exceljs')
 const crypto = require('crypto')
 const fs = require('fs')
 const namesMatters = require('../../../namesMatters.json')
+const dinero = require('dinero.js')
+
+dinero.globalLocale = 'pt-br'
 
 documents.post('/declaration', async (req, res) => {
     const { id, frequencia: frequência } = req.body
@@ -154,6 +157,180 @@ documents.post('/declaration', async (req, res) => {
             res.setHeader('Content-type', 'application/pdf')
             res.end(Buffer.concat(chunks))
         })
+    } else {
+        res.status('400').json({error: 'Id inválido'})
+    }
+})
+
+documents.post('/declaration-finance', async (req, res) => {
+    const { id, ano } = req.body
+
+    if (mongoose.isValidObjectId(id)) {
+        const aluno = await alunosModels.findById(id)
+
+        if (aluno) {
+            const anoLetivo = (await turmasModels.findById(aluno.turma)).serie
+    
+            const doc = new PDFDOCUMENT({size: 'A4', margin: 60, lang: 'pt-br', displayTitle: `Declaração do aluno(a) ${aluno.nome}`, info: {
+                Title: `Declaração do aluno(a) ${aluno.nome}`,
+                CreationDate: new Date(),
+                Author: 'Sistema Josival Penha',
+                Creator: 'Sistema Josival Penha',
+                ModDate: new Date(),
+                Producer: 'Sistema Josival Penha'
+            }})
+    
+            const chunks = []
+
+            let totalMensalidades = 0
+
+            Object.keys(aluno.pagamentos).map(mês => totalMensalidades+=aluno.pagamentos[mês].valueBruto)
+            
+            totalMensalidades = dinero({ amount: totalMensalidades, currency: 'BRL' }).toFormat()
+
+            doc.name = `Declaração do aluno(a) ${aluno.nome}`
+    
+            doc.on('data', chunk => chunks.push(chunk))
+
+            doc
+            .opacity(0.15)
+            .image(path.resolve(__dirname, '..', '..', '..', 'public', 'logo-josival-penha.png'), 150, 300, {
+                width: 300
+            })
+            .opacity(1)
+            .image(path.resolve(__dirname, '..', '..', '..', 'public', 'logo-josival-penha.png'), 250, 50, {
+                width: 90
+            })
+            .font('Helvetica-Bold')
+            .moveDown(6.5)
+            .text('Instituto Educacional Josival Penha', {
+                align: 'center'
+            })
+            .font('Helvetica')
+            .moveDown(0.4)
+            .text('Cadastro Escolar nº P. 109.212 / INEP nº 26170981', {
+                align: 'center'
+            })
+            .moveDown(0.4)
+            .text('Portaria SEE nº 888 D.O 18/02/2003', {
+                align: 'center'
+            })
+            .moveDown(0.4)
+            .text('CNPJ: 11.654.198/0001-43', {
+                align: 'center'
+            })
+            .fontSize(20)
+            .moveDown(2)
+            .font('Helvetica-Bold')
+            .text('DECLARAÇÃO', {
+                align: 'center',
+                underline: true
+            })
+            .font('Helvetica')
+            .fontSize(13)
+            .moveDown(3)
+            .text(`Paulista, ${new Date().getDate()} de ${data.getMes(new Date().toLocaleDateString('pt-br').split('/')[1])} de ${new Date().       getFullYear()}`, {
+                align: 'right',
+                underline: false
+            })
+            .moveDown(2)
+            .text('Declaro que o aluno(a) ', {
+                continued: true,
+                align: 'justify'
+            })
+            .font('Helvetica-Bold')
+            .text(aluno.nome, {
+                continued: true,
+                align: 'left',
+                underline: true
+            })
+            .font('Helvetica')
+            .text(' Cursou o ', {
+                continued: true,
+                align: 'justify',
+                underline: false
+            })
+            .font('Helvetica-Bold')
+            .text(`${anoLetivo} - em ${ano}`, {
+                continued: true,
+                align: 'left',
+                underline: true
+            })
+            .font('Helvetica')
+            .text(' nesta entidade de ensino.', {
+                align: 'left',
+                underline: false
+            })
+            .text('Srª ', {
+                continued: true
+            })
+            .font('Helvetica-Bold')
+            .text(aluno.responsável1, {
+                continued: true,
+                align: 'left',
+                underline: true
+            })
+            .font('Helvetica')
+            .text(', CPF nº ', {
+                continued: true,
+                underline: false
+            })
+            .font('Helvetica-Bold')
+            .text(aluno.cpf, {
+                continued: true,
+                align: 'left',
+                underline: true
+            })
+            .font('Helvetica')
+            .text(' pai e responsável financeiro pelo aluno(a).', {
+                align: 'left',
+                underline: false
+            })
+            .text(`Está adimplente com as mensalidades escolares no ano de ${ano}.`)
+            .text('Nos seguintes meses:')
+            .text(`Matrícula.................................................${aluno.pagamentos['01'].value.replace('R$ ', '')}`)
+            .text(`Fevereiro.................................................${aluno.pagamentos['02'].value.replace('R$ ', '')}`)
+            .text(`Março......................................................${aluno.pagamentos['03'].value.replace('R$ ', '')}`)
+            .text(`Abril.........................................................${aluno.pagamentos['04'].value.replace('R$ ', '')}`)
+            .text(`Maio.........................................................${aluno.pagamentos['05'].value.replace('R$ ', '')}`)
+            .text(`Junho.......................................................${aluno.pagamentos['06'].value.replace('R$ ', '')}`)
+            .text(`Julho........................................................${aluno.pagamentos['07'].value.replace('R$ ', '')}`)
+            .text(`Agosto......................................................${aluno.pagamentos['08'].value.replace('R$ ', '')}`)
+            .text(`Setembro.................................................${aluno.pagamentos['09'].value.replace('R$ ', '')}`)
+            .text(`Outubro....................................................${aluno.pagamentos['10'].value.replace('R$ ', '')}`)
+            .text(`Novembro.................................................${aluno.pagamentos['11'].value.replace('R$ ', '')}`)
+            .text(`Dezembro.................................................${aluno.pagamentos['12'].value.replace('R$ ', '')}`)
+            .moveDown(0.5)
+            .text(`Total.......................................................${totalMensalidades.replace('R$ ', '')}`)
+            .moveDown(2.2)
+            .fontSize(12)
+            .text('__________________________________', {
+                align: 'center'
+            })
+            .fontSize(14)
+            .moveDown(0.4)
+            .text('Diretora', {
+                align: 'center'
+            })
+            .font('Helvetica-Bold')
+            .fontSize(11)
+            .text('tel. (81) 3437-2618', 100, 752)
+            .text('Av. João Paulo II, 894', 240, 751)
+            .text('www.josivalpenha.com', 385, 750)
+            .text('cel. (81) 99499-7501', 100, 768)
+            .text('Mirueira, Paulista - PE', 240, 767)
+            .text('@josival.penha', 385, 766)
+    
+            doc.end()
+    
+            doc.on('end', () => {
+                res.setHeader('Content-disposition', `inline; filename=Declaração do aluno(a) ${aluno.nome}.pdf`)
+                res.setHeader('Content-type', 'application/pdf')
+                res.end(Buffer.concat(chunks))
+            })
+        } else {
+            res.status('400').json({error: 'Esse aluno(a) não existe'})
+        }
     } else {
         res.status('400').json({error: 'Id inválido'})
     }
