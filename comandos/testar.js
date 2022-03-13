0ee1c91b1479167ca8d65f0a6494daee9030a892
Miguel Penha <miguel.penha1@gmail.com>
2022-03-13 05:28:01 -0300

    Testando

--- comandos/testar.js
+++
@@ -1,7 +0,0 @@
-const port = require('../config/port').port
-const shellJs = require('shelljs')
-
-const users = 20
-const reqs = 200
-
-shellJs.exec(`artillery quick --count ${users} -n ${reqs} http://localhost:${port}`).stdout
